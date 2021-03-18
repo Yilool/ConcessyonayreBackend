cc54cb56cb650a44ed6a4a1e1a361f7a572107dd
Yilool <20010917chen@gmail.com>
2021-03-18 11:15:46 +0100

    First commit for upload the base of the project

---
+++ src/main/java/com/concesionarie/concesyonarye/ConcesyonaryeApplication.java
@@ -0,0 +1,13 @@
+package com.concesionarie.concesyonarye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesyonaryeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesyonaryeApplication.class, args);
+	}
+
+}
